27fc11237ea5acaf482ebeb5921ea65e95882b6b
Hugo <hodestito@gmail.com>
2019-11-01 15:51:07 -0300

    Excluindo imports não usados

--- usuarios/src/main/java/br/com/fiap/g1/eventos/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/br/com/fiap/g1/eventos/usuarios/controller/UsuarioController.java
@@ -4,10 +4,7 @@ import br.com.fiap.g1.eventos.usuarios.model.Usuario;
 import br.com.fiap.g1.eventos.usuarios.repository.UsuarioRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
