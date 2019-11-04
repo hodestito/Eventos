7425291653f2ba19f54ae5cf8145e767e45f7885
Hugo <hodestito@gmail.com>
2019-11-04 09:26:32 -0300

    Atualizacao do docker-compose

--- eventos/src/main/java/br/com/fiap/g1/eventos/eventos/repository/EventoRepository.java
+++ eventos/src/main/java/br/com/fiap/g1/eventos/eventos/repository/EventoRepository.java
@@ -4,11 +4,6 @@ import br.com.fiap.g1.eventos.eventos.model.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EventoRepository extends JpaRepository<Evento, Long> {
-
-    Optional<Evento> findByEmailAndSenha(String email, String senha);
-
 }
